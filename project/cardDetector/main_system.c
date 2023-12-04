44143a5887438bde45270e5dc692587a841353c1
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2023-12-04 21:05:29 +0100

    threadpool

---
+++ project/cardDetector/main_system.c
@@ -0,0 +1,19 @@
+
+int main() {
+
+    // We want to configure some listeners for some signals (Heartbeat)
+    // Case we do not receive a heartbeat in a certain amount of time we will mark the system as failure
+    // We will wait a bit more to receive a heartbeat... case we don't we will restart these subprocesses
+    // We will wait a little bit to se it we receive a heartbeat... case we don't we will retry that work once and again
+
+    // Open a couple of suprocesses:
+    // 1 for the card validator
+    // 1 for the card detector
+
+    // Here is when we want to start listening for heartbeats
+
+
+    // We will turn on them
+
+}
+
